ea7d85ab2b90a5fdf7a0416f3af74999c11f412c
Thomas Zhu <dev@thomaszhu.com>
2018-02-24 23:21:36 -0800

    Complete part II of assignment #3

--- CRNA/App.js
+++ CRNA/App.js
@@ -1,32 +1,165 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image, ScrollView, Dimensions, SafeAreaView, Button, TextInput, Alert, FlatList, Text } from 'react-native';
+
+var members = require('./assets/constants');
+
+function randomHex() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this._onBackgroundColorChange = this._onBackgroundColorChange.bind(this);
+    this._onTextChange = this._onTextChange.bind(this);
+    this._onTextSubmit = this._onTextSubmit.bind(this);
+
+    this.state = {
+        backgroundColor: '#ACDAF4', 
+        secretPhrase: ''
+    };
+  }
+
+  _onBackgroundColorChange() {
+    this.setState({backgroundColor: randomHex()});
+  }
+
+  _onTextChange(text) {
+    this.setState({secretPhrase: text});
+  }
+
+  _onTextSubmit(text) {
+    if (this.state.secretPhrase == 'Thomas' || this.state.secretPhrase == 'thomas') {
+      Alert.alert("Success", "You entered the right phrase!");
+    } else {
+      Alert.alert("Invalid", "You entered the wrong phrase!");
+    }
+  }
+  
+  _renderItem = ({item}) => (
+    <View style={styles.listItem}>
+      <Image style={styles.listAvatar} source={{uri: item.image}} />
+      <Text style={styles.listName}>{item.name}</Text>
+      <Text style={styles.listUsername}>@{item.github_username}</Text>
+    </View>
+  );
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Thomas is <Text style={styles.important}>AWESOME</Text>.</Text>
-        <Text style={styles.subtitle}>Too awesome to describe in words...</Text>
-      </View>
+      <SafeAreaView>
+        <ScrollView style={{backgroundColor: '#4D85DC'}}>
+          <View>
+            <Image style={styles.fullImage} source={require('./images/dog.jpg')} />
+          </View>
+
+          <View style={{flexDirection: 'row'}}>
+            <View style={styles.imageContainer}>
+            <Image style={styles.halfImage} source={require('./images/cat.jpg')} />
+            </View>
+            <View style={styles.imageContainer}>
+            <Image style={styles.halfImage} source={require('./images/bunny.jpg')} />
+            </View>
+          </View>
+
+          <View style={[styles.backgroundColorView, {backgroundColor: this.state.backgroundColor}]}>
+            <Button
+              onPress={this._onBackgroundColorChange}
+              title="Change background color" />
+          </View>
+
+          <View style={styles.inputView}>
+            <TextInput 
+              placeholder="Enter secret phrase"
+              placeholderTextColor="white"
+              style={styles.input}
+              onChangeText={this._onTextChange}
+              value={this.state.secretPhrase}
+              onSubmitEditing={this._onTextSubmit}/>
+          </View>
+
+          <View style={styles.list}>
+            <FlatList
+              keyExtractor={(item, index) => index}
+              data={members}
+              renderItem={this._renderItem} />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  imageContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 30
+    backgroundColor: '#193441',
+    padding: 20, 
+    height: Dimensions.get('window').width / 2
   }, 
-  important: {
-    color: 'red',
+  fullImage: {
+    flex: 1, 
+    resizeMode: 'cover', 
+    width: null, 
+    height: Dimensions.get('window').width / 2
+  }, 
+  halfImage: {
+    flex: 1, 
+    resizeMode: 'cover', 
+    width: null,
+    height: null
+  }, 
+  backgroundColorView: {
+    flex: 1, 
+    height: Dimensions.get('window').width, 
+    justifyContent: 'center'
+  }, 
+  inputView: {
+    flex: 1, 
+    height: 240, 
+    backgroundColor: '#4D85DC', 
+    justifyContent: 'center', 
+    paddingLeft: 40, 
+    paddingRight: 40
+  }, 
+  input: {
+    height: 60,
+    borderWidth: 1,
+    borderColor: 'white', 
+    borderRadius: 10, 
+    paddingLeft: 15,
+    paddingRight: 15
+  }, 
+  list: {
+    flex: 1, 
+    height: 750
+  }, 
+  listItem: {
+    padding: 10, 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    height: 50, 
+    flex: 1, 
+    flexDirection: 'row', 
+    backgroundColor: 'white'
+  }, 
+  listAvatar: {
+    width: 40, 
+    height: 40, 
+    borderRadius: 20
+  }, 
+  listName: {
+    paddingLeft: 10, 
+    flex: 1, 
+    color: '#5B6D85', 
     fontWeight: 'bold'
-  },
-  subtitle: {
+  }, 
+  listUsername: {
     color: 'gray'
   }
 });
---
+++ CRNA/assets/constants.js
@@ -0,0 +1,79 @@
+var COMMUNITY_MEMBERS = [
+    {
+      name: 'Monte Thakkar',
+      image: 'https://avatars2.githubusercontent.com/u/7840686?s=400&v=4',
+      github_username: 'monte9'
+    },
+    {
+      name: 'Ryan Liszewski',
+      image: 'https://avatars3.githubusercontent.com/u/5275250?s=460&v=4',
+      github_username: 'ryanliszewski'
+    },
+    {
+      name: 'Thomas Zhu',
+      image: 'http://thomaszhu.com/resources/thomas_avatar.jpeg',
+      github_username: 'thomashzhu'
+    },
+    {
+      name: 'Bhavesh Chowdhury',
+      image: 'https://avatars3.githubusercontent.com/u/13439148?s=460&v=4',
+      github_username: 'bhaveshc789'
+    },
+    {
+      name: 'Sukhjit Singh',
+      image: 'https://avatars1.githubusercontent.com/u/18251293?s=400&u=1ee2922f2dd90d94bb4efbec7cc815ef510a0ad7&v=4',
+      github_username: 'sukhjitsingh'
+    },
+    {
+      name: 'Prakash Gurung',
+      image: 'https://avatars.githubusercontent.com/makkhay',
+      github_username: 'makkhay'
+    },
+    {
+      name: 'Nicholas Szeto',
+      image: 'https://avatars1.githubusercontent.com/u/23561635?s=460&v=4',
+      github_username: 'niszeto'
+    },
+    {
+      name: 'Emanuel Saunders',
+      image: 'https://scontent-lax3-2.cdninstagram.com/vp/1ee634d2bdf29d5a327858a15ccfdf42/5B26F2E8/t51.2885-19/s150x150/18160691_1198139890329419_5383374010954285056_a.jpg',
+      github_username: 'saundemanu'
+    },
+    {
+      name: 'William Hua',
+      image: 'https://avatars3.githubusercontent.com/u/36139326?s=460&v=4',
+      github_username: 'william-hua'
+    },
+    {
+      name: 'Mitul Savani',
+      image: 'https://scontent.fsnc1-1.fna.fbcdn.net/v/t31.0-8/18556546_1553987881278489_849408995015005895_o.jpg?oh=bb65219475a9cbac85ca7090e7ee877b&oe=5ADE398B',
+      github_username: 'mitulsavani'
+    },
+    {
+      name: 'Jakhongir Khusanov',
+      image: 'https://avatars0.githubusercontent.com/u/25942541?s=460&v=4',
+      github_username: 'jkhusanov'
+    },
+    {
+      name: 'Teodora Caneva',
+      image: 'https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-1/c44.0.320.320/p320x320/20914645_1579156895438534_1027285013181250707_n.jpg?oh=0ff1b3c4c8bc8e00edb84cecd660d0fe&oe=5B1C897A',
+      github_username: 'kodekat'
+    },
+    {
+      name: 'Affaan Ghazzali',
+      image: 'https://avatars0.githubusercontent.com/u/32111151?s=460&v=4',
+      github_username: 'affaanghazzali'
+    },
+    {
+      name: 'Girish Rawat',
+      image: 'https://avatars2.githubusercontent.com/u/6697235?s=460&v=4',
+      github_username: 'GirishRawat'
+    },
+    {
+      name: 'Karan Gupta',
+      image: 'https://avatars1.githubusercontent.com/u/19352928?s=460&v=4',
+      github_username: 'karanguptak9'
+    }
+  ];
+
+  module.exports = COMMUNITY_MEMBERS;
